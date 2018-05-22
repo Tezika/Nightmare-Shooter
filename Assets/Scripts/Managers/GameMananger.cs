3e83365f748818df14bce561095adeefd474769e
Tezika <zfk565614653@outlook.com>
2018-05-22 15:53:09 +0800

    modify the name of prototype level

--- Assets/Scripts/Managers/GameMananger.cs
+++ Assets/Scripts/Managers/GameMananger.cs
@@ -19,14 +19,17 @@ namespace Shooter.Game
 		public  AudioClip _vicClip;
 		public  AudioClip _failClip;
 		public  AudioClip _btnClip;
-		private AudioSource _audio;
+        private AudioSource _audio;
+
 		void Awake(){
 			this._phc = GameObject.FindGameObjectWithTag ("Player").gameObject.GetComponent<PlayerHPCtrl> ();
 			this._psc = GameObject.FindGameObjectWithTag ("Player").gameObject.GetComponentInChildren<PlayerShootingCtrl> ();
 			this._pmc = GameObject.FindGameObjectWithTag ("Player").gameObject.GetComponent<PlayerMoveCtrl> ();
 			this._eManager = GameObject.FindGameObjectWithTag ("EnemyManager").gameObject.GetComponent<EnemyManager> ();
 			this._audio = GetComponent<AudioSource> ();
+
 		}
+
 		void OnEnable(){
 			this._phc.onPlayerDead += HandleonPlayerDead;
 			this._eManager.onBossDead += HandleonBossDead;
@@ -46,6 +49,7 @@ namespace Shooter.Game
 			this.playEffectSound (this._failClip);
 			StartCoroutine (waitForOver (waitingTime,"GameOver"));
 		}
+
 		IEnumerator waitForOver(float waitingTime,string aniClipTriggerName){
 			yield return new WaitForSeconds (waitingTime);
 			canvas.GetComponent<Animator>().SetTrigger(aniClipTriggerName);
@@ -55,15 +59,18 @@ namespace Shooter.Game
 			canvas.GetComponent<CanvasGroup> ().interactable = true;
 			canvas.GetComponent<CanvasGroup> ().blocksRaycasts = true;
 		}
+
 		void OnDisable(){
 			this._phc.onPlayerDead -= HandleonPlayerDead;
 			this._eManager.onBossDead -= HandleonBossDead;
 		}
+
 		void playEffectSound(AudioClip clip){
 			this._audio.clip = clip;
 			this._audio.Stop ();
 			this._audio.Play ();
 		}
+
 		public void onClickRestart(){
 			this.playEffectSound (this._btnClip);
 			Application.LoadLevel (Application.loadedLevel);
--- Assets/Scripts/UI/UIBtnCtrl.cs
+++ Assets/Scripts/UI/UIBtnCtrl.cs
@@ -2,29 +2,35 @@
 using System.Collections;
 namespace Shooter.UI
 {
-	public class UIBtnCtrl : MonoBehaviour {
-		private Animator _ani;
-		private AudioSource _audio;
-		void Awake(){
-			this._ani = this.GetComponent<Animator> ();
-			this._audio = this.GetComponent<AudioSource> ();
-		}
-		void playBtnSound(){
-			this._audio.Stop ();
-			this._audio.Play ();
-		}
-		public void onClickStart(){
-			this.playBtnSound ();
-			this._ani.SetTrigger("GameStart");
-		}
-		public void onClickQuit(){
-			this.playBtnSound ();
-			Application.Quit ();
-		}
-		public void onClickLetsGo(){
-			this.playBtnSound ();
-			Application.LoadLevel (1);
-		}
-	}
+    public class UIBtnCtrl : MonoBehaviour
+    {
+        private Animator _ani;
+        private AudioSource _audio;
+        void Awake()
+        {
+            this._ani = this.GetComponent<Animator>();
+            this._audio = this.GetComponent<AudioSource>();
+        }
+        void playBtnSound()
+        {
+            this._audio.Stop();
+            this._audio.Play();
+        }
+        public void onClickStart()
+        {
+            this.playBtnSound();
+            this._ani.SetTrigger("GameStart");
+        }
+        public void onClickQuit()
+        {
+            this.playBtnSound();
+            Application.Quit();
+        }
+        public void onClickLetsGo()
+        {
+            this.playBtnSound();
+            Application.LoadLevel(1);
+        }
+    }
 
 }
